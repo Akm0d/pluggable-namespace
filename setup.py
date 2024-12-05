6458d8364231ee8c3d68c8a3eb53f5e0ec075143
Tyler Levy Conde <yonstib@gmail.com>
2024-12-04 17:17:06 -0700

    working in editable mode

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
+from pathlib import Path
 from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
-# Define Cython extensions
+# Define the extensions
 extensions = [
     Extension(name="pns.contract", sources=["src/pns/contract.pyx"]),
     Extension(name="pns.data", sources=["src/pns/data.pyx"]),
 ]
 
-# Call setup function
+# Directory where the setup.py is located
+SETUP_DIRNAME = Path(__file__).parent
+
+
 setup(
     ext_modules=cythonize(extensions),
 )
-
-# TODO calculate "full" dependencies and also gather dependencies dynamically from what imports are used in config.yamls
-# Also have extras based on each subproject name
