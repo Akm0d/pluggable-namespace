f52014106625a01364886dd88f3b96a1514c2f19
Tyler Levy Conde <yonstib@gmail.com>
2024-12-04 18:15:47 -0700

    remove redundancies

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
-# Define the extensions
 extensions = [
     Extension(name="pns.contract", sources=["src/pns/contract.pyx"]),
     Extension(name="pns.data", sources=["src/pns/data.pyx"]),
 ]
 
-# Directory where the setup.py is located
 SETUP_DIRNAME = Path(__file__).parent
 
 
