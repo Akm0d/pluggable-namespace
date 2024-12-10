3be909c112fe4ea4b7c4d2f7859f725f9a4ed496
Tyler Levy Conde <yonstib@gmail.com>
2024-12-09 22:41:56 -0700

    rename dynamic

--- src/pns/hub.py
+++ src/pns/hub.py
@@ -66,7 +66,7 @@ class Sub(pns.data.Namespace):
 class Hub(Sub):
     _last_ref = None
     _last_call = None
-    dynamic = None
+    _dynamic = None
 
     def __init__(hub):
         super().__init__(name="hub", parent=None, root=None)
@@ -74,7 +74,7 @@ class Hub(Sub):
         # Add a place for sys modules to live
         hub += "lib"
         hub.lib.data = sys.modules
-        hub.dynamic = pns.dir.dynamic()
+        hub._dynamic = pns.dir.dynamic()
 
 
 
@@ -87,7 +87,7 @@ async def new(cli:str="cli", *args, **kwargs):
 
     # Load the config
     await hub.add_sub("config", "pns.config")
-    opt = await hub.pns.config.load(cli=cli, **hub.dynamic.config)
+    opt = await hub.pns.config.load(cli=cli, **hub._dynamic.config)
     hub.OPT = pns.data.NamespaceDict(opt)
 
     await hub.add_sub("log", "pns.log")
